2be323164ec5433df097ab3a0dd3058b8de2664c
beezebrown <m.brown298@gmail.com>
2016-01-09 02:16:02 -0500

    Initialized projet and added podfile

---
+++ Ink/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ink
+//
+//  Created by Marc Brown on 1/9/16.
+//  Copyright © 2016 Puricode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
